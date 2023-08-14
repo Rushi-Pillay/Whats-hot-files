742f1e69004b40280b9b7096c46a333e5804bd67
Ruan <Ruan-vr@outlook.com>
2023-08-14 14:48:08 +0200

    First Commit, Just after create Personal Acccount

---
+++ app/src/main/java/com/example/createpersonalaccount/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.createpersonalaccount;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
